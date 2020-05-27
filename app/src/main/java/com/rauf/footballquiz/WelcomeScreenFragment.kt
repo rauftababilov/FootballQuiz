ad0ea07b800a4b25a1ae8308d803ce9eeb0e61fd
rauf tababilov <tababilovrauf@gmail.com>
2020-05-27 18:55:49 +0500

    add Navigation and NavHost

--- app/src/main/java/com/rauf/footballquiz/WelcomeScreenFragment.kt
+++ app/src/main/java/com/rauf/footballquiz/WelcomeScreenFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.rauf.footballquiz.databinding.ActivityMainBinding
 import com.rauf.footballquiz.databinding.FragmentWelcomeScreenBinding
 
@@ -19,9 +20,13 @@ class WelcomeScreenFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding : FragmentWelcomeScreenBinding = DataBindingUtil.inflate(
+        val binding :  FragmentWelcomeScreenBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_welcome_screen, container, false
         )
+
+        binding.letsPlayButton.setOnClickListener { view: View ->
+            Navigation.findNavController(view).navigate(R.id.action_welcomeScreenFragment_to_quizFragment)
+        }
 
         return binding.root;
     }
